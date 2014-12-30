88e5a3eba132666059b926aed7d39014ebe8bedb
Sandy Chapman <schapman@lixar.com>
2014-12-30 08:18:09 -0400

    Updated setup.py to add new dependencies and upped the version number.

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+CHANGELOG
+=========
+
+Work Outstanding
+----------------
+- Change for RAML file header
+- Some file structures from yaml still not converted to python class objects.
+- Tests
+
+Version 0.1.1
+-------------
+- Added python dependencies to setup.py
+
+Version 0.1
+-----------
+- Initial library commit.
+- Basic RAML support
--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from setuptools import setup
 
 setup(name='ramlizer',
-      version='0.1',
+      version='0.1.1',
       description='A parser for the RAML (RESTful API Modeling Language) file format.',
       url='https://github.com/SandyChapman/ramlizer',
       author='Sandy Chapman',
       author_email='sandychapman@gmail.com',
       license='MIT',
       packages=['ramlizer'],
+      install_requires=['yaml', 'json', 'jsonschema'],
       zip_safe=False)
